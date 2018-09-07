a68c69f9539e45e38a5c7cdacc0cd0d6e3c10059
Rifhice <rifhice@MSI.localdomain>
2018-09-07 19:59:49 +0200

    zoehf

---
+++ Routes/DB.js
@@ -0,0 +1,20 @@
+import { resolve } from "dns";
+import { rejects } from "assert";
+
+var db = {
+  "1": { googleId: "105798007328638690051", faceboookId: "id" },
+  "2": { googleId: "id", faceboookId: "id" }
+};
+
+var getUserWithGoogleID = userId => {
+  return new Promise((resolve, reject) => {
+    for (user of db) {
+      if (db[user].googleId === userId) {
+        resolve(db[user]);
+      }
+    }
+    reject();
+  });
+};
+
+module.exports = { getUserWithGoogleID };
--- Routes/Login.js
+++ Routes/Login.js
@@ -1,8 +1,9 @@
 var express = require("express");
 const router = express.Router();
 const { OAuth2Client } = require("google-auth-library");
+const DB = require("./DB.js");
 
-router.post("/", function(req, res) {
+router.post("/Google", function(req, res) {
   let token = req.body.token;
   const client = new OAuth2Client(
     "466608887201-cs254tau34sd5k7s0kisafitb6boptek.apps.googleusercontent.com"
@@ -16,18 +17,18 @@ router.post("/", function(req, res) {
     const payload = ticket.getPayload();
     const userid = payload["sub"];
     console.log(userid);
-    // If request specified a G Suite domain:
-    //const domain = payload['hd'];
+    DB.getUserWithGoogleID(userid)
+      .then(result => {
+        console.log(result);
+        res.send({ code: 202 });
+      })
+      .catch(err => {
+        //User not in the database
+      });
   }
-  verify()
-    .then(result => {
-      console.log(result);
-      res.send("login");
-    })
-    .catch(err => {
-      console.log(err);
-      res.send("error");
-    });
+  verify().catch(err => {
+    res.send({ code: 401 });
+  });
 });
 
 module.exports = router;
